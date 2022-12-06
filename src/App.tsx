0c493f348e300664a8cfd1b6c2ccc8426e88b5f4
Alissom Franco <badeluiza592@gmail.com>
2022-12-06 20:22:37 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+
 
 function App() {
+  const [city, setCity] = useState("")
+  const [weatherData, setWeatherData] = useState<any>({})
+  // const [icon, setIcon] = useState("")
+
+
+  const handleSearch = () => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?appid=49b33b68191e4c1248e5763aafd8b662&lat=30&lon=30&units=metric`).then(response => {
+      console.log(response.data)
+      setWeatherData(response.data)
+    })
+  }
+
+
+  const handleChange = (e: any) => {
+    console.log(e.target.value)
+    setCity(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <a className="navbar-brand text-white">
+          Weather Forecast
         </a>
-      </header>
+      </nav>
+      
+      <main className="container">
+      <div className="jumbotrom">
+          <h1>Check the weather forecast here!</h1>
+          <p className="lead">        
+            Write your city in the field below and click search
+          </p>
+
+          <div className="row mb-4">
+            <div className="col-md-6">
+              <input
+                onChange={handleChange}
+                className="form-control"
+                value={city} />
+            </div>
+          </div>
+
+          <button onClick={handleSearch} className="btn btn-primary">
+            Search
+          </button>
+          
+          
+          <div>
+            <blockquote className="blockquote">
+              {
+                weatherData.main && <p className="mb-0">The current temperature is:{weatherData.main.temp}°F</p>
+              }
+            </blockquote>
+          </div>
+
+          <div>
+            <blockquote className="blockquote">
+              {
+                weatherData.weather && <p className="mb-0">The sky is: {weatherData.weather[0]?.description}</p>
+              }
+            </blockquote>
+          </div>
+          
+        </div>
+      </main>
     </div>
   );
 }
